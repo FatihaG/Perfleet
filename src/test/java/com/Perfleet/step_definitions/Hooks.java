7e6b10cb7f56b2c33b01d7b6e5dd2db1d263eafe
Ellie-tpl <elifetoplu@yahoo.com>
2021-02-22 12:19:55 +0000

    add login feature

--- src/test/java/com/Perfleet/step_definitions/Hooks.java
+++ src/test/java/com/Perfleet/step_definitions/Hooks.java
@@ -4,6 +4,9 @@ import com.Perfleet.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -23,5 +26,22 @@ public class Hooks {
             scenario.attach(screenshot,"image/png","screenshot");
         }
         Driver.closeDriver();
+    }
+    public class LoginStepDefs{
+        @Given("the user is on the login page")
+        public void the_user_is_on_the_login_page() throws InterruptedException {
+            Thread.sleep(5000);
+            System.out.println("I open the browser and navigate to perfleet login page");
+        }
+        @When("the user enters the driver information")
+        public void the_user_enters_the_driver_information() throws InterruptedException {
+                Thread.sleep(3000);
+                System.out.println("I write down driver information");
+        }
+        @Then("the user should be able to login")
+        public void the_user_should_be_able_to_login() throws InterruptedException {
+            Thread.sleep(3000);
+            System.out.println("I should be able to login");
+        }
     }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,2 +1,16 @@
 
-  Feature:
+  Feature: Users should be able to login
+    Scenario login as a driver
+    Given the user on login page
+    When the user given driver information
+    Then the user should be able to login
+
+    Scenario login as a sales manager
+      Given the user on login page
+      When the user given sales manager information
+      Then the user should be able to login
+
+    Scenario login as a store manager
+      Given the user on login page
+      When the user given store manager information
+      Then the user should be able to login
