6819747097c1578a5d714af7c7022cde6bfa2254
Andy D <36055273+andussu@users.noreply.github.com>
2021-02-25 14:36:06 +0200

    added frame of BrowseCarsInfo

---
+++ src/test/java/com/Perfleet/step_definitions/VehicleBrowseCarsInfoStepDefs.java
@@ -0,0 +1,10 @@
+package com.Perfleet.step_definitions;
+
+import io.cucumber.java.en.And;
+
+public class VehicleBrowseCarsInfoStepDefs {
+    @And("the user clicks on record {int} in table")
+    public void theUserClicksOnRecordInTable(int pageNumber) {
+
+    }
+}
