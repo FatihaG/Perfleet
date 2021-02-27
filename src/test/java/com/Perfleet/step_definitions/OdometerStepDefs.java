946ddd03768ec5d648c1fdc5210dc2430092f68e
abdulkadirbatibay <72045695+abdulkadirbatibay@users.noreply.github.com>
2021-02-27 18:16:20 +0100

    Update OdometerStepDefs.java

--- src/test/java/com/Perfleet/step_definitions/OdometerStepDefs.java
+++ src/test/java/com/Perfleet/step_definitions/OdometerStepDefs.java
@@ -9,6 +9,7 @@ public class OdometerStepDefs {
 
     @When("the user selects {int} st option")
     //static 
+    //public not main
     public void the_user_selects_st_option(int index) {
         BrowserUtils.waitFor(3);
         new OdometerPage().clickOnOptionWithIndex(index);
